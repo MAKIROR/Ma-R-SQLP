9d0b3a4e1e9b6d0c8861e257326af505ee588f2c
MAKIROR <87136269+MAKIROR@users.noreply.github.com>
2023-04-22 21:20:22 +0800

    Delete target directory

--- target/debug/deps/lexer-b013605c0ee678ff.d
+++
@@ -1,5 +0,0 @@
-/home/makiror/work/Ma-R-SQLP/target/debug/deps/lexer-b013605c0ee678ff: tests/lexer.rs
-
-/home/makiror/work/Ma-R-SQLP/target/debug/deps/lexer-b013605c0ee678ff.d: tests/lexer.rs
-
-tests/lexer.rs:
